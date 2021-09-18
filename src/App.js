de2026249bbd3779473054ed4b7ab0a79ee5789c
barti132 <barteksredzinski@gmail.com>
2021-09-19 01:51:27 +0200

    add: orbits & saturn ring

--- src/App.js
+++ src/App.js
@@ -1,74 +1,41 @@
 import './App.css';
 
-import React, {Suspense, useRef, useState} from "react";
+import React, {Suspense} from "react";
 
-import {Canvas, useFrame, useLoader} from "@react-three/fiber";
-import {Html, OrbitControls, Stars} from "@react-three/drei";
-import {TextureLoader} from "three";
-import ReactDOM from "react-dom";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, Stars} from "@react-three/drei";
+import {Planet, Saturn} from "./Planet";
 
-function Sphere(props) {
-    const texture = useLoader(TextureLoader, props.texture)
-    const [active, setActive] = useState(false)
-    const ref = useRef()
 
-    useFrame((state, delta) => (ref.current.rotation.y += 0.01))
-
+function Lights() {
     return (
-        <mesh
-            {...props}
-            ref={ref}
-            onClick={(e) => {
-                const element = active ? (
-                    <div id="text"><h1>Lorem ipsum dolor</h1> sit amet, consectetur adipiscing elit. Pellentesque vel ipsum ac
-                        ligula rhoncus blandit. Aliquam tempus blandit iaculis. Phasellus non dignissim magna. Interdum
-                        et malesuada fames ac ante ipsum primis in faucibus. Pellentesque a interdum odio. Vestibulum dignissim
-                        sem a mi blandit, quis aliquet eros suscipit. Phasellus neque metus, tristique at diam a, malesuada
-                        pharetra augue. Duis venenatis, lorem ut mattis tristique, tortor metus vestibulum felis, id hendrerit
-                        felis nibh posuere sapien. Suspendisse quis lacus orci.
-                        <h1>Phasellus vitae porta purus</h1>, a placerat leo. Fusce ultrices, dolor id sagittis ultricies, magna
-                        lectus mattis nulla, sit amet pretium ante nunc ac lectus. Ut rutrum tellus nibh, nec malesuada
-                        ante laoreet ac. Quisque id orci et libero egestas ultricies ut in sem. Pellentesque condimentum,
-                        metus id posuere tempus, ipsum ipsum rhoncus risus, et congue ante nisi vitae mi. Cras dignissim
-                        in ante et posuere. Vivamus molestie tortor id neque pretium, nec interdum nunc pulvinar. Vestibulum
-                        vehicula felis eu nulla pulvinar tincidunt. Integer nibh sem, malesuada id ultrices accumsan, vulputate
-                        eget nunc. Vivamus interdum elit et urna laoreet, et iaculis diam volutpat. Nulla eu laoreet quam,
-                        in ultrices augue. Phasellus tempus egestas diam, id dignissim nibh. Donec tincidunt quis nisi id
-                        interdum. Proin convallis tempor felis et viverra. Cras feugiat sollicitudin nisi.
-                    </div>) : (<div></div>)
-                setActive(!active)
-                ReactDOM.render(element, document.getElementById("planetInfo"))
-            }}
-        >
-            <Html distanceFactor={15}>
-                <div id="annotation">{props.name}</div>
-            </Html>
-            <sphereGeometry args={[props.size, 32, 32]}/>
-            <meshStandardMaterial map={texture}/>
-        </mesh>
+        <>
+            <ambientLight/>
+            <pointLight position={[0, 0, 0]}/>
+        </>
     )
 }
 
-function Lights(){
-    return(
+function Planets() {
+    return (
         <>
-            <ambientLight/>
-            <pointLight position={[0, 0, 0]} />
-        </>)
-}
-
-function Planets(){
-    return(
-        <>
-            <Sphere position={[0, 0, 0]} size={5.45} texture={'2k_sun.jpg'} name={'Sun'}/>
-            <Sphere position={[15, 0, 0]} size={0.19125} texture={'2k_mercury.jpg'} name={'Mercury'}/>
-            <Sphere position={[20, 0, 0]} size={0.47445} texture={'2k_venus_atmosphere.jpg'} name={'Venus'}/>
-            <Sphere position={[25, 0, 0]} size={0.5} texture={'2k_earth_daymap.jpg'} name={'Earth'}/>
-            <Sphere position={[30, 0, 0]} size={0.26675} texture={'2k_mars.jpg'} name={'Mars'}/>
-            <Sphere position={[45, 0, 0]} size={5.6046} texture={'2k_jupiter.jpg'} name={'Jupiter'}/>
-            <Sphere position={[65, 0, 0]} size={4.7247} texture={'2k_saturn.jpg'} name={'Saturn'}/>
-            <Sphere position={[80, 0, 0]} size={2.0037} texture={'2k_uranus.jpg'} name={'Uranus'}/>
-            <Sphere position={[95, 0, 0]} size={1.94135} texture={'2k_neptune.jpg'} name={'Neptune'}/>
+            <Planet position={[0, 0, 0]} size={5.45} speed={0.2} zRadius={0} texture={'2k_sun.jpg'} name={'Sun'}/>
+            <Planet position={[10.7909, 0, 0]} size={0.19125} speed={0.2} zRadius={8} texture={'2k_mercury.jpg'}
+                    name={'Mercury'}/>
+            <Planet position={[15.8208, 0, 0]} size={0.47445} speed={0.2} zRadius={13}
+                    texture={'2k_venus_atmosphere.jpg'} name={'Venus'}/>
+            <Planet position={[19.9597, 0, 0]} size={0.5} speed={0.2} zRadius={17} texture={'2k_earth_daymap.jpg'}
+                    name={'Earth'}/>
+            <Planet position={[27.7936, 0, 0]} size={0.26675} speed={0.2} zRadius={25} texture={'2k_mars.jpg'}
+                    name={'Mars'}/>
+            <Planet position={[82.8412, 0, 0]} size={5.6046} speed={0.2} zRadius={78} texture={'2k_jupiter.jpg'}
+                    name={'Jupiter'}/>
+            <Saturn position={[147.6725, 0, 0]} size={4.7247} speed={0.2} zRadius={140} texture={'2k_saturn.jpg'}
+                    name={'Saturn'}/>
+            <Planet position={[292.0972, 0, 0]} size={2.0037} speed={0.2} zRadius={280} texture={'2k_uranus.jpg'}
+                    name={'Uranus'}/>
+            <Planet position={[454.8252, 0, 0]} size={1.94135} speed={0.2} zRadius={440} texture={'2k_neptune.jpg'}
+                    name={'Neptune'}/>
         </>
     )
 }
@@ -76,10 +43,10 @@ function Planets(){
 export default function App() {
     return (
         <div id="root">
-            <Canvas camera={{fov: 75, position: [0,0,40]}}>
+            <Canvas camera={{fov: 75, position: [0, 0, 40]}}>
                 <color attach="background" args={"black"}/>
                 <Suspense fallback={null}>
-                    <Stars radius={250} depth={50} count={5000} factor={10} saturation={0} fade />
+                    <Stars radius={300} depth={50} count={5000} factor={10} saturation={0} fade/>
                     <Lights/>
                     <Planets/>
                     <OrbitControls/>
---
+++ src/Planet.js
@@ -0,0 +1,171 @@
+import {useFrame, useLoader} from "@react-three/fiber";
+import * as THREE from "three";
+import {TextureLoader} from "three";
+import React, {useRef, useState} from "react";
+import ReactDOM from "react-dom";
+import {Html} from "@react-three/drei";
+
+export function Planet(props) {
+    const texture = useLoader(TextureLoader, props.texture)
+    const [active, setActive] = useState(false)
+    const ref = useRef()
+
+    useFrame(({clock}) => {
+        const time = clock.getElapsedTime() * props.speed
+        const x = props.position[0] * Math.sin(time)
+        const z = props.zRadius * Math.cos(time)
+        ref.current.position.x = x
+        ref.current.position.z = z
+    })
+
+    return (
+        <>
+            <mesh
+                {...props}
+                ref={ref}
+                onClick={(e) => {
+                    const element = active ? (
+                        <div id="text"><h1>Lorem ipsum dolor</h1> sit amet, consectetur adipiscing elit. Pellentesque
+                            vel ipsum ac
+                            ligula rhoncus blandit. Aliquam tempus blandit iaculis. Phasellus non dignissim magna.
+                            Interdum
+                            et malesuada fames ac ante ipsum primis in faucibus. Pellentesque a interdum odio.
+                            Vestibulum dignissim
+                            sem a mi blandit, quis aliquet eros suscipit. Phasellus neque metus, tristique at diam a,
+                            malesuada
+                            pharetra augue. Duis venenatis, lorem ut mattis tristique, tortor metus vestibulum felis, id
+                            hendrerit
+                            felis nibh posuere sapien. Suspendisse quis lacus orci.
+                            <h1>Phasellus vitae porta purus</h1>, a placerat leo. Fusce ultrices, dolor id sagittis
+                            ultricies, magna
+                            lectus mattis nulla, sit amet pretium ante nunc ac lectus. Ut rutrum tellus nibh, nec
+                            malesuada
+                            ante laoreet ac. Quisque id orci et libero egestas ultricies ut in sem. Pellentesque
+                            condimentum,
+                            metus id posuere tempus, ipsum ipsum rhoncus risus, et congue ante nisi vitae mi. Cras
+                            dignissim
+                            in ante et posuere. Vivamus molestie tortor id neque pretium, nec interdum nunc pulvinar.
+                            Vestibulum
+                            vehicula felis eu nulla pulvinar tincidunt. Integer nibh sem, malesuada id ultrices
+                            accumsan, vulputate
+                            eget nunc. Vivamus interdum elit et urna laoreet, et iaculis diam volutpat. Nulla eu laoreet
+                            quam,
+                            in ultrices augue. Phasellus tempus egestas diam, id dignissim nibh. Donec tincidunt quis
+                            nisi id
+                            interdum. Proin convallis tempor felis et viverra. Cras feugiat sollicitudin nisi.
+                        </div>) : (<div></div>)
+                    setActive(!active)
+                    ReactDOM.render(element, document.getElementById("planetInfo"))
+                }}
+            >
+                <Html distanceFactor={15}>
+                    <div id="annotation">{props.name}</div>
+                </Html>
+                <sphereGeometry args={[props.size, 32, 32]}/>
+                <meshStandardMaterial map={texture}/>
+            </mesh>
+            <Ellipse xRadius={props.position[0]} zRadius={props.zRadius}/>
+        </>
+    )
+}
+
+export function Saturn(props) {
+    const texture = useLoader(TextureLoader, props.texture)
+    const ringTexture = useLoader(TextureLoader, "2k_saturn_ring_alpha.png")
+    const [active, setActive] = useState(false)
+    const ref = useRef()
+
+    useFrame(({clock}) => {
+        const time = clock.getElapsedTime() * props.speed
+        const x = props.position[0] * Math.sin(time)
+        const z = props.zRadius * Math.cos(time)
+        ref.current.position.x = x
+        ref.current.position.z = z
+    })
+
+    return (
+        <>
+            <mesh
+                {...props}
+                ref={ref}
+                onClick={(e) => {
+                    const element = active ? (
+                        <div id="text"><h1>Lorem ipsum dolor</h1> sit amet, consectetur adipiscing elit. Pellentesque
+                            vel ipsum ac
+                            ligula rhoncus blandit. Aliquam tempus blandit iaculis. Phasellus non dignissim magna.
+                            Interdum
+                            et malesuada fames ac ante ipsum primis in faucibus. Pellentesque a interdum odio.
+                            Vestibulum dignissim
+                            sem a mi blandit, quis aliquet eros suscipit. Phasellus neque metus, tristique at diam a,
+                            malesuada
+                            pharetra augue. Duis venenatis, lorem ut mattis tristique, tortor metus vestibulum felis, id
+                            hendrerit
+                            felis nibh posuere sapien. Suspendisse quis lacus orci.
+                            <h1>Phasellus vitae porta purus</h1>, a placerat leo. Fusce ultrices, dolor id sagittis
+                            ultricies, magna
+                            lectus mattis nulla, sit amet pretium ante nunc ac lectus. Ut rutrum tellus nibh, nec
+                            malesuada
+                            ante laoreet ac. Quisque id orci et libero egestas ultricies ut in sem. Pellentesque
+                            condimentum,
+                            metus id posuere tempus, ipsum ipsum rhoncus risus, et congue ante nisi vitae mi. Cras
+                            dignissim
+                            in ante et posuere. Vivamus molestie tortor id neque pretium, nec interdum nunc pulvinar.
+                            Vestibulum
+                            vehicula felis eu nulla pulvinar tincidunt. Integer nibh sem, malesuada id ultrices
+                            accumsan, vulputate
+                            eget nunc. Vivamus interdum elit et urna laoreet, et iaculis diam volutpat. Nulla eu laoreet
+                            quam,
+                            in ultrices augue. Phasellus tempus egestas diam, id dignissim nibh. Donec tincidunt quis
+                            nisi id
+                            interdum. Proin convallis tempor felis et viverra. Cras feugiat sollicitudin nisi.
+                        </div>) : (<div></div>)
+                    setActive(!active)
+                    ReactDOM.render(element, document.getElementById("planetInfo"))
+                }}
+            >
+                <Html distanceFactor={15}>
+                    <div id="annotation">{props.name}</div>
+                </Html>
+                <sphereGeometry args={[props.size, 32, 32]}/>
+                <meshStandardMaterial map={texture}/>
+                <SaturnRing position={[props.position[0], 0, props.position[2]]} texture={ringTexture}/>
+
+            </mesh>
+            <Ellipse xRadius={props.position[0]} zRadius={props.zRadius}/>
+
+        </>
+    )
+}
+
+function SaturnRing(props) {
+    const ref = new THREE.TorusGeometry(6, 0.5, 64, 64)
+    ref.rotateX(Math.PI / 2)
+    ref.position = new THREE.Vector3(props.position[0], 0, props.position[2])
+
+    return (
+        <mesh geometry={ref}>
+            <meshStandardMaterial map={props.texture}/>
+        </mesh>
+    )
+}
+
+function Ellipse({xRadius = 1, zRadius = 1}) {
+    const points = []
+
+    for (let i = 0; i < 64; i++) {
+        const angle = (i / 64) * 2 * Math.PI
+        const x = xRadius * Math.cos(angle)
+        const z = zRadius * Math.sin(angle)
+        points.push(new THREE.Vector3(x, 0, z))
+    }
+    points.push(points[0])
+
+    const ellipse = new THREE.BufferGeometry().setFromPoints(points)
+
+
+    return (
+        <line geometry={ellipse}>
+            <lineBasicMaterial attach="material" color="#bfbbda" linewidth={10}/>
+        </line>
+    )
+}
